8a289f53f75327c832d94c0c21796b90f0a5b90b
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-03 10:57:32 +0200

    finishing the login screen ui

--- lib/presentation/screens/login.dart
+++ lib/presentation/screens/login.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import '../widgets/login/login_body.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: LoginBody(),
+    );
   }
 }
---
+++ lib/presentation/widgets/login/login_body.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/utils/assets.dart';
+import '../../../core/utils/styles.dart';
+import 'login_container.dart';
+
+class LoginBody extends StatelessWidget {
+  const LoginBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Image.asset(
+          AssetsPaths.login,
+          width: size.width,
+          height: size.height,
+          fit: BoxFit.fill,
+        ),
+        Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'My',
+                  style: Styles.title,
+                ),
+                const Text(
+                  'Gallery',
+                  style: Styles.title,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                LoginContainer(size: size),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}

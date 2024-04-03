304f9b4497fd636a0c09e27b7dc725dbf7fa5b8e
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-03 10:56:21 +0200

    adding a styles.dart folder to easy access all styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const titleMedium = TextStyle(
+      fontSize: 34, color: Color(0xff4A4A4A), fontWeight: FontWeight.w900);
+
+  static const title = TextStyle(
+      fontSize: 50, color: Color(0xff4A4A4A), fontWeight: FontWeight.w700);
+
+  static const subTitle = TextStyle(
+      fontSize: 18, color: Colors.white, fontWeight: FontWeight.w900);
+
+}

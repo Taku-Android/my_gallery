d42f4c4f39f02a8b4646b7f6b1d21d6af402bdfd
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 08:15:03 +0200

    adding custom widget for loading and error

---
+++ lib/core/utils/custom_loading_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomLoadingWidget extends StatelessWidget {
+  const CustomLoadingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+      ),
+    );
+  }
+}

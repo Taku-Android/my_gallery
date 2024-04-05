d8706e7142a23fad5f9f7056cc2eae9ac19acbef
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 08:14:40 +0200

    adding end points

--- lib/core/api/apiEndpoints.dart
+++ lib/core/api/apiEndpoints.dart
@@ -4,6 +4,8 @@ const String BASE_URL = 'https://flutter.prominaagency.com/api';
 
 // AUTH
 const String LOGIN_API = '$BASE_URL/auth/login';
+const String UPLOAD_IMAGE_API = '$BASE_URL/upload';
+const String GET_IMAGES_API = '$BASE_URL/my-gallery';
 
 
 

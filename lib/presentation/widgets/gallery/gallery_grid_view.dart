d97f6ff841fa33a1db7b5461d88e7e1fdfbdd993
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 08:18:07 +0200

    listen to the image cubit

--- lib/presentation/widgets/gallery/gallery_grid_view.dart
+++ lib/presentation/widgets/gallery/gallery_grid_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_gallery/core/utils/custom_error_widget.dart';
+import 'package:my_gallery/core/utils/custom_loading_widget.dart';
+import 'package:my_gallery/presentation/bloc/images_bloc/images_cubit.dart';
+import 'package:my_gallery/presentation/bloc/images_bloc/images_state.dart';
 import 'gallery_list_item.dart';
 
 class GalleryGridView extends StatelessWidget {
@@ -6,16 +11,28 @@ class GalleryGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 30,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // 3 items per row
-        crossAxisSpacing: 4.0, // adjust as needed
-        mainAxisSpacing: 4.0, // adjust as needed
-      ),
-      itemBuilder: (BuildContext context, int index) {
-        // replace this with your actual list item widget
-        return const GalleryListItem();
+    return BlocBuilder<ImagesCubit, ImagesState>(
+      builder: (context, state) {
+        if(state is ImagesFailure){
+          return CustomErrorWidget(errMessage: state.errMsg);
+        }else if(state is ImagesSuccess){
+          var imageList = state.getImagesModel.data!.images;
+          return GridView.builder(
+            itemCount: imageList!.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 3 items per row
+              crossAxisSpacing: 7.0, // adjust as needed
+              mainAxisSpacing: 7.0, // adjust as needed
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              // replace this with your actual list item widget
+              return  GalleryListItem(image: imageList[index]);
+            },
+          );
+        }else{
+          return const CustomLoadingWidget();
+        }
+
       },
     );
   }

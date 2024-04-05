bbb4a008d8ea1c8fe94305ce8e517ff11fb966cd
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 05:06:38 +0200

    adding images to assets.dart

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,7 +2,13 @@ class AssetsPaths{
 
 
   static const login = 'assets/images/login.png';
+  static const gallery = 'assets/images/gellary.png';
+  static const user = 'assets/images/user.png';
+
+  static const logout = 'assets/images/logout.png';
+  static const upload = 'assets/images/upload.png';
 
 
+  static const pic1 = 'assets/images/pic1.png';
 
 }

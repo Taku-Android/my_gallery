084ce5bf025aa451d921d17dddf3d8b9f07dad70
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 05:55:23 +0200

    listen to cubit to login

--- lib/presentation/widgets/login/login_body.dart
+++ lib/presentation/widgets/login/login_body.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_gallery/presentation/bloc/login_bloc/login_cubit.dart';
+import 'package:my_gallery/presentation/bloc/login_bloc/login_state.dart';
 
 import '../../../core/utils/assets.dart';
+import '../../../core/utils/dialog_helper.dart';
 import '../../../core/utils/styles.dart';
+import '../../screens/gallery_screen.dart';
 import 'login_container.dart';
 
 class LoginBody extends StatelessWidget {
@@ -10,36 +15,59 @@ class LoginBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Image.asset(
-          AssetsPaths.login,
-          width: size.width,
-          height: size.height,
-          fit: BoxFit.fill,
-        ),
-        Center(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 Text(
-                  'My',
-                  style: Styles.title,
-                ),
-                 Text(
-                  'Gallery',
-                  style: Styles.title,
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                LoginContainer(size: size),
-              ],
+    return BlocListener<LoginCubit, LoginState>(
+      listener: (context, state) {
+        if (state is LoginLoading) {
+          DialogHelper.showLoadingDialog(context);
+        }
+        if (state is LoginFailure) {
+          Navigator.pop(context);
+          DialogHelper.showInfoDialog(context, state.errMsg, onOkPressed: () {
+            Navigator.pop(context);
+          });
+        }
+        if (state is LoginSuccess) {
+          DialogHelper.hideLoadingDialog();
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) =>
+                  GalleryScreen(user: state.loginResponse.user!),
+            ),
+          );
+        }
+      },
+      child: Stack(
+        children: [
+          Image.asset(
+            AssetsPaths.login,
+            width: size.width,
+            height: size.height,
+            fit: BoxFit.fill,
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'My',
+                    style: Styles.title,
+                  ),
+                  Text(
+                    'Gallery',
+                    style: Styles.title,
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  LoginContainer(size: size),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }

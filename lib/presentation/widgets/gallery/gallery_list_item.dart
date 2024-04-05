37681feee8c14d1c64528de98129882aeaa0757d
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 08:17:23 +0200

    change gallery_list_item.dart to be cahchedNetworkImage to fetch it from url

--- lib/presentation/widgets/gallery/gallery_list_item.dart
+++ lib/presentation/widgets/gallery/gallery_list_item.dart
@@ -1,17 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../core/utils/assets.dart';
+import '../../../core/utils/custom_loading_widget.dart';
 
 class GalleryListItem extends StatelessWidget {
-  const GalleryListItem({super.key});
+  const GalleryListItem({super.key, required this.image});
+
+  final String image ;
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Image.asset(
-      AssetsPaths.pic1,
-      width: size.width / 4,
-      fit: BoxFit.fill,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(24),
+      child: CachedNetworkImage(
+        imageUrl: image,
+        fit: BoxFit.fill,
+        height: size.height / 10,
+        width: size.width / 2.5,
+        errorWidget: (context, value, _) => Image.asset(AssetsPaths.error),
+        placeholder: (context, value) => const CustomLoadingWidget(),
+      ),
     );
   }
 }

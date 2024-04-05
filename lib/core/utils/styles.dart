e1d71ab38fcab0407bbe41e2ad50f71913afdcb9
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 05:05:53 +0200

    refactor sizes

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
-      fontSize: 34, color: Color(0xff4A4A4A), fontWeight: FontWeight.w900);
+  static final titleMedium = TextStyle(
+      fontSize: 34.sp, color: const Color(0xff4A4A4A), fontWeight: FontWeight.w900);
 
-  static const title = TextStyle(
-      fontSize: 50, color: Color(0xff4A4A4A), fontWeight: FontWeight.w700);
+  static final  title = TextStyle(
+      fontSize: 50.sp, color: const Color(0xff4A4A4A), fontWeight: FontWeight.w700);
 
-  static const subTitle = TextStyle(
-      fontSize: 18, color: Colors.white, fontWeight: FontWeight.w900);
+  static final subTitle = TextStyle(
+      fontSize: 16.sp, color: Colors.white, fontWeight: FontWeight.w900);
 
 }

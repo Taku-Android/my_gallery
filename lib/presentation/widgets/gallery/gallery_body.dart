0c6ba44c8d604ca0cb72279ac1f3d5ba1d4f3409
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 07:49:33 +0200

    listen to the upload states

--- lib/presentation/widgets/gallery/gallery_body.dart
+++ lib/presentation/widgets/gallery/gallery_body.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_gallery/presentation/bloc/upload_bloc/upload_cubit.dart';
+import 'package:my_gallery/presentation/bloc/upload_bloc/upload_state.dart';
 
 import '../../../core/utils/assets.dart';
+import '../../../core/utils/dialog_helper.dart';
 import '../../../data/model/User.dart';
 import 'gallery_buttons.dart';
 import 'gallery_grid_view.dart';
@@ -9,35 +13,56 @@ import 'gallery_user_info.dart';
 class GalleryBody extends StatelessWidget {
   const GalleryBody({super.key, required this.user});
 
-  final User user ;
+  final User user;
 
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Image.asset(
-          AssetsPaths.gallery,
-          width: size.width,
-          height: size.height,
-          fit: BoxFit.fill,
-        ),
-         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0 , ),
-          child: Column(
-            children: [
-              GalleryUserInfo(user: user),
-              const SizedBox(height: 40,),
-              const GalleryButtons(),
-              const SizedBox(height: 30,),
-              const Expanded(child: GalleryGridView()),
-
-            ],
+    Size size = MediaQuery
+        .of(context)
+        .size;
+    return BlocListener<UploadCubit, UploadState>(
+      listener: (context, state) {
+        if (state is UploadLoading) {
+          DialogHelper.showLoadingDialog(context);
+        }
+        if (state is UploadFailure) {
+          Navigator.pop(context);
+          DialogHelper.showInfoDialog(context, state.errMsg, onOkPressed: () {
+            Navigator.pop(context);
+          });
+        }
+        if (state is UploadSuccess) {
+          DialogHelper.hideLoadingDialog();
+          DialogHelper.showInfoDialog(context, state.loginResponse.message!, onOkPressed: () {
+            Navigator.pop(context);
+          });
+        }
+      },
+      child: Stack(
+        children: [
+          Image.asset(
+            AssetsPaths.gallery,
+            width: size.width,
+            height: size.height,
+            fit: BoxFit.fill,
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0,),
+            child: Column(
+              children: [
+                GalleryUserInfo(user: user),
+                const SizedBox(height: 40,),
+                const GalleryButtons(),
+                const SizedBox(height: 30,),
+                const Expanded(child: GalleryGridView()),
 
-      ],
+              ],
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }

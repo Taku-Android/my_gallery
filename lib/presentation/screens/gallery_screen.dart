f9da41feb214536d7ef574e6a41eb1c7eb770ab0
Ahmed Ahmed Ibrahim <93339810+Taku-Android@users.noreply.github.com>
2024-04-05 08:16:45 +0200

    provide images cubit

--- lib/presentation/screens/gallery_screen.dart
+++ lib/presentation/screens/gallery_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_gallery/core/service_locator.dart';
 import 'package:my_gallery/data/model/User.dart';
+import 'package:my_gallery/presentation/bloc/images_bloc/images_cubit.dart';
 import 'package:my_gallery/presentation/bloc/upload_bloc/upload_cubit.dart';
 import '../widgets/gallery/gallery_body.dart';
 
@@ -12,8 +13,15 @@ class GalleryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => UploadCubit(getIt()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider (
+          create: (BuildContext context) => UploadCubit(getIt()),
+        ),
+        BlocProvider (
+          create: (BuildContext context) => ImagesCubit(getIt())..getImages(),
+        ),
+      ],
       child: Scaffold(
         body: SafeArea(child: GalleryBody(user: user)),
       ),
